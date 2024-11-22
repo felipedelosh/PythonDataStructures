547487bc78ac874660d8438ca0c0d2fbdbcad61a
felipedelosh <doctorfhernandez@hotmail.com>
2024-11-22 14:03:47 -0500

    WIP: DLL ADD & SHOW

--- 2 - Doubly Linked List/DoubleLikedList.py
+++ 2 - Doubly Linked List/DoubleLikedList.py
@@ -7,3 +7,43 @@ class DoubleLinkedList:
     def __init__(self) -> None:
         self.pivot = None
 
+
+    def addDataStart(self, data):
+        new_node = Node(data)
+        if self.pivot == None:
+            self.pivot = new_node
+        else:
+            new_node.next = self.pivot
+            self.pivot.previous = new_node
+            self.pivot = new_node
+
+    
+    def addDataEnd(self, data):
+        new_node = Node(data)
+        if self.pivot == None:
+            self.pivot = new_node
+        else:
+            _copy = self.pivot
+
+            while _copy.next != None:
+                _copy = _copy.next
+
+            _copy.next = new_node
+            new_node = _copy
+            
+
+    def showAllData(self):
+        if self.pivot != None:
+            print(f"Pivote: {self.pivot.data}")
+
+            _cpyN = self.pivot.next
+            _cpyP = self.pivot.previous
+
+            while _cpyN != None:
+                print(f"NEXT: {_cpyN.data}")
+                _cpyN = _cpyN.next
+
+            while _cpyP != None:
+                print(f"Previous: {_cpyP.data}")
+                _cpyP = _cpyP.previous
+
--- 2 - Doubly Linked List/main.py
+++ 2 - Doubly Linked List/main.py
@@ -6,3 +6,10 @@ Demostration of double linked List
 from DoubleLikedList import DoubleLinkedList
 
 list = DoubleLinkedList()
+
+list.addDataStart(1)
+list.addDataStart(2)
+list.addDataStart(3)
+
+
+list.showAllData()
