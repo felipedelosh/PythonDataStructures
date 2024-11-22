0b18705ecc0efc045d0b174189d5764e01dd47dc
felipedelosh <doctorfhernandez@hotmail.com>
2024-11-22 13:50:18 -0500

    WIP: DLL

--- 2 - Doubly Linked List/main.py
+++ 2 - Doubly Linked List/main.py
@@ -6,18 +6,3 @@ Demostration of double linked List
 from DoubleLikedList import DoubleLinkedList
 
 list = DoubleLinkedList()
-
-#list.addDataNextUpdatingPivot(1)
-list.addDataPreviousUpdatingPivot(6)
-#list.addDataNextUpdatingPivot(2)
-list.addDataPreviousUpdatingPivot(7)
-#list.addDataNextUpdatingPivot(3)
-list.addDataPreviousUpdatingPivot(8)
-#list.addDataNextUpdatingPivot(4)
-list.addDataPreviousUpdatingPivot(9)
-#list.addDataNextUpdatingPivot(5)
-
-list.showAllDataNext()
-print("=================")
-list.showAllDataPrevious()
-
