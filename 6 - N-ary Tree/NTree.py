d3d8de0c8b44b6ff795976aa148a455068efc771
felipedelosh <doctorfhernandez@hotmail.com>
2024-12-11 21:50:00 -0500

    NTree

--- 6 - N-ary Tree/NTree.py
+++ 6 - N-ary Tree/NTree.py
@@ -13,8 +13,11 @@ class NTree:
             if father != None:
                 self.root = Node(father)
 
-            if data != None:
+            if father != None and data != None:
                 self._addData(self.root, father, data)
+
+            if father == None and data != None:
+                self.root = Node(data)
         else:
             self._addData(self.root, father, data)
     def _addData(self, pivot, father, data):
@@ -23,7 +26,49 @@ class NTree:
         else:
             for i in pivot.children:
                 self._addData(i, father, data)
+
+
+    def deleteNode(self, father, data):
+        if self.root != None and father != None and data != None:
+            self._deleteNode(self.root, father, data)
+    def _deleteNode(self, pivot, father, data):
+        if pivot.data == father:
+            pivot.children = [node for node in pivot.children if node.data != data]
+        else:   
+            for i in pivot.children:
+                self._deleteNode(i, father, data)
+
+
+    def searchByData(self, data):
+        if self.root != None:
+            return self._searchByData(self.root, data)
+        
+        return None
+    def _searchByData(self, pivot, data):
+        if pivot.data == data:
+            return pivot
+        
+        for i in pivot.children:
+            result = self._searchByData(i, data)
+
+            if result != None:
+                return result
+        
+        return None
     
+
+    def getTreeHeight(self):
+        if self.root != None:
+            return self._getTreeHeight(self.root)
+        
+        return 0
+    def _getTreeHeight(self, pivot):
+        if pivot.children != []:
+            _height_register = [self._getTreeHeight(i) for i in pivot.children]
+            return 1 + max(_height_register)
+        else:
+            return 1
+
 
     def view(self):
         self._view(self.root)
@@ -33,3 +78,22 @@ class NTree:
             
             for i in pivot.children:
                 self._view(i)
+
+    def viewPreOrder(self):
+        self._viewPreOrder(self.root)
+    def _viewPreOrder(self, pivot):
+        if pivot != None:
+            print(pivot.data)
+            
+            for i in pivot.children:
+                self._viewPreOrder(i)
+
+
+    def viewPostOrder(self):
+        self._viewPostOrder(self.root)
+    def _viewPostOrder(self, pivot):
+        if pivot != None:
+            for i in pivot.children:
+                self._viewPostOrder(i)
+
+            print(pivot.data)
