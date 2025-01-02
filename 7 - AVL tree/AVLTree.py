8c492ff365c1d92c41da08ef8b31818dfe0693f8
felipedelosh <doctorfhernandez@hotmail.com>
2025-01-02 17:24:42 -0500

    WIP: DOC insert

--- 7 - AVL tree/AVLTree.py
+++ 7 - AVL tree/AVLTree.py
@@ -11,25 +11,18 @@ class AVLTree:
     def addData(self, data):
         self.root = self._addData(self.root, data)
     def _addData(self, pivot, data):
-        print(f"Entra a guardar: {data} >> PIVOT: {pivot}")
         if pivot == None:
             new_node = Node(data)
-            print(f"Se a creado el nodo:{new_node}>>{new_node.data}")
             return new_node
         if pivot.data < data:
             pivot.right = self._addData(pivot.right, data)
         else:
             pivot.left = self._addData(pivot.left, data)
         
-        print(f"continua el {pivot.data} su der: {pivot.right} su izq: {pivot.left}")
-
         pivot.height = 1 + max(self.getNodeHeight(pivot.left), self.getNodeHeight(pivot.right))
-        print(f"Para el NODO: {pivot.data}, su altura es:{pivot.height}")
         f_equilibrium = self.getNodeEquilibrium(pivot)
-        print(f"para el nodo{pivot.data} {pivot} su equilibrio es: {f_equilibrium}")
 
         if f_equilibrium > 1:
-            print("+ROTACION...")
             if data < pivot.left.data:
                 return self.RR(pivot)
             else:
@@ -37,18 +30,12 @@ class AVLTree:
                 return self.RR(pivot)
 
         if f_equilibrium < -1:
-            print("-ROTACION...")
             if data > pivot.right.data:
-                print("Caso data es mayor que pivot")
                 return self.RL(pivot)
             else:
-                print("Caso data es menor que pivot")
                 pivot.right = self.RR(pivot.right)
                 return self.RL(pivot)
-
-        print(f"FINAL:{pivot}  >> {pivot.data}")
-        print(f"DER: {pivot.right}")
-        print(f"IZQ: {pivot.left}")
+            
         return pivot
 
 
@@ -60,19 +47,18 @@ class AVLTree:
         node.left = T3
         node.height = 1 + max(self.getNodeHeight(node.left), self.getNodeHeight(node.right))
         y.height = 1 + max(self.getNodeHeight(y.left), self.getNodeHeight(y.right))
+
         return y
 
 
     def RL(self, node):
-        print(f"Entra a rotar: El nodo{node.data}")
         y = node.right
         T2 = y.left
         y.left = node
         node.right = T2
         node.height = 1 + max(self.getNodeHeight(node.left), self.getNodeHeight(node.right))
-        print(node.height)
         y.height = 1 + max(self.getNodeHeight(y.left), self.getNodeHeight(y.right))
-        print(y.height)
+        
         return y
 
 
